011344c28755fa6f8939fc1b12de5f7c96aa5bda
paalss <paalss94@gmail.com>
2021-07-26 11:38:26 +0200

    CSSTransition comp som legger spesielle classnames til <div className="Modal">

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,30 +1,21 @@
 import React from "react";
-import Transition from "react-transition-group/Transition";
+import CSSTransition from "react-transition-group/CSSTransition";
 
 import "./Modal.css";
 
 const modal = (props) => {
   return (
-    <Transition mountOnEnter unmountOnExit in={props.show} timeout={300}>
-      {(state) => {
-        const cssClasses = [
-          "Modal",
-          state === "entering"
-            ? "ModalOpen"
-            : state === "exiting"
-            ? "ModalClosed"
-            : null,
-        ];
-        return (
-          <div className={cssClasses.join(" ")}>
+    <CSSTransition mountOnEnter unmountOnExit in={props.show} timeout={300} classNames="fade-slide">
+      {(state) => (
+          <div className="Modal">
             <h1>A Modal</h1>
             <button className="Button" onClick={props.closed}>
               Dismiss
             </button>
           </div>
-        );
-      }}
-    </Transition>
+        )
+      }
+    </CSSTransition>
   );
 };
 
